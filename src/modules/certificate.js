3c88c09185e5e6108d7f6be36b0576453e38dfe0
Igor Golubev <igruliman@gmail.com>
2022-02-17 11:30:10 +0300

    middle diplom add work

--- src/modules/certificate.js
+++ src/modules/certificate.js
@@ -4,34 +4,40 @@ const certificate = () => {
 
     const modalImg = document.createElement('div');
     modalImg.classList.add('header-modal', 'header-modal--opened');
-    modalImg.style.cssText='max-height: 100%; margin: 30px;'
+    modalImg.style.cssText='max-width: 42%;'
     document.body.append(modalImg);
 
     const btnClose = document.querySelector('.header-modal__close')
     const btnImg = btnClose.cloneNode(true);
+    btnImg.style.cssText='background: #4f686e; padding: 8px; border-radius: 50%; color: #fff;'
     modalImg.append(btnImg);
 
     const docResource = document.querySelector('.sertificate-document .img-responsive')
     const docImg = docResource.cloneNode(true);
     docImg.src = "images/documents/original/document4.jpg"
-    docImg.style.cssText=''
-    
     modalImg.append(docImg)
 
     //Условие
     btns.forEach((btn) => {
       btn.addEventListener("click", (e) => {
         e.preventDefault();
-
-        modalImg.style.display = 'block';
-
-    
-        
+        modalImg.style.display = 'flex';
         overlay.style.display = "block";
       });
     });
   
     // Событие по клику
+
+    btnImg.addEventListener('mouseenter', function(e){ 
+      e.target.style.transition = '1s';
+      e.target.style.background = '#5d5d5d'; // Выставляем цвет
+    });
+    
+    btnImg.addEventListener('mouseleave', function(e){
+      e.target.style.transition = '1s';
+      e.target.style.background = '#4f686e'; 
+    });
+
     btnImg.addEventListener("click", () => {
         modalImg.style.display = 'none';
           overlay.style.display = "none";
