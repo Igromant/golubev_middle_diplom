6315e8c1b1fc460af2ec25fa2a9e167285100e2c
Igor Golubev <igruliman@gmail.com>
2022-02-24 06:51:01 +0300

    middle diplom add work

--- src/modules/comments.js
+++ src/modules/comments.js
@@ -1,12 +1,5 @@
 const comments = () => {
-    // fetch('https://moddle-diploma-default-rtdb.firebaseio.com/comments.json')
-    // .then(response => response.json())
-    // .then(data => {
-    //     console.log(data);
-    // })
-    // .catch(error => {
-    //     console.log(error)
-    // })
+    
 }
 
 export default comments
